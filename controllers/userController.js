4ffddb50606e44582215c8e5bdce210b188ab08b
shahzeb <shahzeb.algoace@gmail.com>
2023-08-25 16:31:21 +0500

    login and signup with authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,35 @@
-exports.getAllUsers=(req,res) =>{
-    res.status(500).json({
-        status: 'error',
-        message:'This route is not yet defined!'
-    })
-}
+//const fs= require('fs');
+//const sendErrorProd= require('./errorController')
+//const AppError = require('../utils/appError');
+const User= require('./../models/userModel');
+//const APIFeatures=require('./../utils/apiFeatures');
+const catchAsync=require('./../utils/catchAsync');
+
+exports.getAllUsers=catchAsync(async(req,res,next) =>{
+    const users= await User.find();
+    //query.sort().select().skip().limit()
+
+    //const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy')
+
+    // SEND RESPONSE
+
+    //console.log(req.requestTime);
+    res.status(200).json({
+        status:'success',
+        //requestedAt: req.requestTime
+         results: users.length,
+         data: {
+             users
+         }
+
+
+
+
+
+        
+    });
+
+})
 
 exports.deleteUser=(req,res) =>{
     res.status(500).json({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,8 @@ const authController= require('./../controllers/authController')
 const router= express.Router();
 
 router.post('/signup', authController.signup)
+router.post('/login', authController.login)
+
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
