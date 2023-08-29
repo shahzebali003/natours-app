f4cd92cce1c984f6e16c9e339210b19082af07c7
shahzeb <shahzeb.algoace@gmail.com>
2023-08-29 16:19:38 +0500

    updated with delete current user functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
+const AppError = require('./../utils/appError')
 //const fs= require('fs');
 //const sendErrorProd= require('./errorController')
 //const AppError = require('../utils/appError');
 const User= require('./../models/userModel');
 //const APIFeatures=require('./../utils/apiFeatures');
 const catchAsync=require('./../utils/catchAsync');
+
+
+const filterObj=(obj, ...allowedFields)=>{
+    const newObj={}
+    Object.keys(obj).forEach(el=>{
+        if(allowedFields.includes(el)) newObj[el]=obj[el]
+    })
+    return newObj
+}
+
 
 exports.getAllUsers=catchAsync(async(req,res,next) =>{
     const users= await User.find();
@@ -30,6 +41,48 @@ exports.getAllUsers=catchAsync(async(req,res,next) =>{
     });
 
 })
+
+
+exports.updateMe=catchAsync(async(req,res,next)=>{
+    // 1) Create error if user post password data
+
+    if(req.body.password || req.body.passwordConfirm){
+        return next(new AppError('This route is not for password updates. Please use /updateMyPassword', 400))
+    }
+    
+
+    // 2) Filtered out unwanted fields names that are not allowed to be updated
+    const filteredBody = filterObj(req.body, 'name', 'email' )
+
+    
+    //2) Update user document
+
+    const updatedUser = await User.findByIdAndUpdate(req.user.id,filteredBody,{
+        new:true, 
+        runValidators: true
+    
+    });
+    // user.name='Jonas'
+    // await user.save();
+
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
+    })
+})
+
+exports.deleteMe = catchAsync(async(req,res,next)=>{
+    await User.findByIdAndUpdate(req.user.id, {active: false})
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
+
 
 exports.deleteUser=(req,res) =>{
     res.status(500).json({
--- models/userModel.js
+++ models/userModel.js
@@ -43,7 +43,12 @@ const userSchema= new mongoose.Schema({
     },
     passwordChangedAt: Date,
     passwordResetToken: String,
-    passwordResetExpires: Date
+    passwordResetExpires: Date,
+    active:{
+        type: Boolean,
+        default: true,
+        select: false
+    }
 
 })
 
@@ -69,6 +74,15 @@ userSchema.pre('save', function(next){
 
     this.passwordChangedAt=Date.now()-1000;
     next() 
+})
+
+
+userSchema.pre(/^find/, function(next){
+
+    // This points to the current query
+    this.find({active: {$ne: false}})
+    next()
+
 })
 
 
