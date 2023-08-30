2fd361a59933b011e8ee604c17135359697e357d
shahzeb <shahzeb.algoace@gmail.com>
2023-08-30 14:02:53 +0500

    updated with all security

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // REQUIRES
 const express= require('express');
 const morgan= require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
+
 const { get } = require('http');
 const AppError=require('./utils/appError')
 const app = express();
@@ -8,21 +15,62 @@ const tourRouter= require('./routes/tourRoutes')
 const userRouter= require('./routes/userRoutes')
 const globalErrorHandler=require('./controllers/errorController')
 // MIDDLEWARES
-//console.log(process.env.NODE_ENV)
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
-}
 
+
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+//console.log(process.env.NODE_ENV)
+
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
+  
+
+// Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+  });
+  app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+    hpp({
+      whitelist: [
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+      ]
+    })
+  );
 
 //app.use(morgan('dev'));
-app.use(express.json());
-// FOR STATIC FILES
+//app.use(express.json());
+// SERVING STATIC FILES
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req,res,next)=>{
 //     console.log('Hello from the middleware!!')
 //     next();
 // });
+
+// TEST MIDDLE WARE
 
 app.use((req,res,next)=>{
     req.requestTime= new Date().toISOString();
